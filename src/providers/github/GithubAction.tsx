0ef5655a0b5766228dcb05a026423b7bbc1b125b
Danial Khakbaz <danialkhakbaz.dk@gmail.com>
2022-01-27 19:24:36 +0330

    FEAT: Fix the CORS Problem

--- src/providers/github/GithubAction.tsx
+++ src/providers/github/GithubAction.tsx
@@ -1,26 +1,20 @@
 export const getUser = async (username: string) => {
-  const response = await fetch(
-    `${process.env.REACT_APP_GITHUB_URL}/users/${username}`,
-    {
-      headers: {
-        Authentication: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    }
-  );
+  const response = await fetch(`/users/${username}`, {
+    headers: {
+      Authentication: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+    },
+  });
   const data = await response.json();
 
   return data;
 };
 
 export const getUserRepos = async (username: string) => {
-  const response = await fetch(
-    `${process.env.REACT_APP_GITHUB_URL}/users/${username}/repos`,
-    {
-      headers: {
-        Authentication: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    }
-  );
+  const response = await fetch(`/users/${username}/repos`, {
+    headers: {
+      Authentication: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+    },
+  });
   const data = await response.json();
 
   return data;
@@ -31,14 +25,11 @@ export const searchUsers = async (searchValue: string) => {
     q: searchValue,
   });
 
-  const response = await fetch(
-    `${process.env.REACT_APP_GITHUB_URL}/search/users?${params}`,
-    {
-      headers: {
-        Authentication: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    }
-  );
+  const response = await fetch(`/search/users?${params}`, {
+    headers: {
+      Authentication: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+    },
+  });
   const { items } = await response.json();
 
   return items;
