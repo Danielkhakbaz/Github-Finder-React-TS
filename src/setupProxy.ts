bc2e9b69b7b94e175dac8c07d8a0f37708308fee
amirhossein khakbaz <danialkhakbaz.dk@gmail.com>
2022-05-30 17:52:30 +0430

    FIX: Fix the code

--- src/setupProxy.ts
+++ src/setupProxy.ts
@@ -3,14 +3,14 @@ export const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = (app: any) => {
   app.use(
     createProxyMiddleware("/users/", {
-      target: "https://api.github.com",
+      target: "https://github-finder-react-ts.vercel.app",
       changeOrigin: true,
       credentials: true,
     })
   );
   app.use(
     createProxyMiddleware("/search/users", {
-      target: "https://api.github.com",
+      target: "https://github-finder-react-ts.vercel.app",
       changeOrigin: true,
       credentials: true,
     })
